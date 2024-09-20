d53a6c122e5ccdae20248bffe66d6b984135254b
Vini78Silva <enzovinisil@gmail.com>
2024-09-20 07:53:12 -0300

    insert feito

--- cliente.php
+++ cliente.php
@@ -2,7 +2,7 @@
 //Recebendo os dados do formulario
 
 $nome = $_POST['nome'];
-$contato = $_POST['contato'];
+$sobrenome = $_POST['sobrenome'];
 $email = $_POST['email'];
 $telefone = $_POST['telefone'];
 $endereco = $_POST['endereco'];
@@ -11,10 +11,10 @@ $endereco = $_POST['endereco'];
 //trazendo o script da conexão que está no arquivo conexao.php 
 include 'conexao.php';
 
-$insert_fornecedor = "INSERT INTO fornecedor
-         VALUES (NULL, '$nome', '$contato', '$email', '$telefone', '$endereco')";
+$insert_cliente = "INSERT INTO cliente
+         VALUES (NULL, '$nome', '$sobrenome', '$email', '$telefone', '$endereco')";
 
-$resultado = $conexao->query($insert_fornecedor);
+$resultado = $conexao->query($insert_cliente);
 
 if ($resultado) {
     echo "<script>alert('Inserido com sucesso'); history.back() </script>";
